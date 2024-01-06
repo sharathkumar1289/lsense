6db9a4b91b6d5f84375c78deebca3b3daa622eca
SHARATH KUMAR BALNE <sharathkumar@Sharaths-MacBook-Air.local>
2024-01-06 16:05:01 +0530

    reform

---
+++ skillscrolls/tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        trueGray: colors.trueGray,
+        primaryPurple: '#A9A5EF',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    // Add other plugins as needed
+  ],
+};
